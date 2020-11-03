26750e2c4a388df82ed00fa669348e1751d2bafb
larryzodiac <evanmachale@gmail.com>
2020-11-03 14:52:37 +0000

    Refresh

--- src/clock.js
+++ src/clock.js
@@ -1,23 +1,23 @@
 (function () {
-  function checkTime(i) {
-    return (i < 10) ? "0" + i : i;
-  }
-
-  function startTime() {
-    var today = new Date(),
-    h = checkTime(today.getHours()),
-    m = checkTime(today.getMinutes()),
-    s = checkTime(today.getSeconds()),
-    d = today.getDate(),
-    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-    mo = months[today.getMonth()],
-    y = today.getFullYear();
-    if(document.getElementById('clock')) {
-     document.getElementById('clock').innerHTML = h + ":" + m + ":" + s + "<br/>" + d + ", " + mo + ", " + y + "<br/> Kildare";
+    function checkTime(i) {
+        return (i < 10) ? "0" + i : i;
     }
-    let t = setTimeout(function () {
-      startTime()
-    }, 500);
-  }
-  startTime();
+
+    function startTime() {
+        var today = new Date(),
+            h = checkTime(today.getHours()),
+            m = checkTime(today.getMinutes()),
+            s = checkTime(today.getSeconds()),
+            d = today.getDate(),
+            months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+            mo = months[today.getMonth()],
+            y = today.getFullYear();
+        if (document.getElementById('clock')) {
+            document.getElementById('clock').innerHTML = h + ":" + m + ":" + s + "<br/>" + d + ", " + mo + ", " + y + "<br/> Kildare";
+        }
+        let t = setTimeout(function () {
+            startTime()
+        }, 500);
+    }
+    startTime();
 })();
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'bundle.css',
+                        },
+                    },
+                    { loader: 'extract-loader' },
+                    { loader: 'css-loader' },
+                    { loader: 'sass-loader' },
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.(png|gif|jpg|jpeg|woff|woff2|eot|ttf|svg)$/,
+                use: [
+                    { loader: 'url-loader?limit=100000' },
+                    { loader: 'file-loader' },
+                ]
+            },
+        ]
+    },
+    devServer: {
+        port: 3000,
+        open: true,
+        historyApiFallback: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            favicon: './public/favicon.ico',
+        }),
+    ]
+};
